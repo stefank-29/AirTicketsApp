83667e7ef69db954e28a25b72a57d43bd47a2dfc
kalugege <kalugege@gmail.com>
2020-12-16 20:59:53 +0100

    resetPass

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(
 
 //? Passport JS is what we use to handle our logins
 app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.session());
 
 // The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
 app.use(flash());
