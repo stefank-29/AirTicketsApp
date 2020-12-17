b69743cf68533cf797f3cf30a81ff089963f7aec
stefan <stefankaraferovic@gmail.com>
2020-12-17 19:38:04 +0100

    admin service

---
+++ AdminService/models/Airplane.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+
+const airplaneSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: 'Please supply airplane name.',
+    },
+    capacity: {
+        type: Number,
+        required: 'Please supply ',
+    },
+});
