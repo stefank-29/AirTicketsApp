59f9e5db29c3764626390201cded429332cfd234
kalugege <kalugege@gmail.com>
2020-12-22 16:25:32 +0100

    servis dva

--- TicketService/routes/index.js
+++ TicketService/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const adminController = require('../controllers/adminController');
 
-router.get('/admin', (req, res) => {
+
+router.get('/tickets', (req, res) => {
     res.render('layout');
 });
 
-router.get('/admin/add/airplane', adminController.addAirplaneForm);
+
 
 module.exports = router;
